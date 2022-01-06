9715aab63997a1e0e3a5831eabfe85ae51431099
taemath <hirotaka081630@gmail.com>
2022-01-06 10:03:31 +0900

    feat(36-6): bigQuery用に記述を修正

--- 36/6.sql
+++ 36/6.sql
@@ -1,10 +1,17 @@
 SELECT
   person,
-  SUBSTRING(
-    'DOB'
-    FROM
-      sum(position(role IN 'DO')) FOR 1
-  )
+  -- substrng( 'DOB' from sum(position (role in 'DO')) for 1  )
+  substr(
+    'DOB',
+    sum(
+      CASE
+        WHEN contains_substr(role, 'D') THEN 1
+        WHEN contains_substr(role, 'O') THEN 2
+        ELSE 0
+      END
+    ),
+    1
+  ) AS office
 FROM
   dataset.Roles
 WHERE
